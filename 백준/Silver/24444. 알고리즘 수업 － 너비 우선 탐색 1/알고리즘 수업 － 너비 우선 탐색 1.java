45b49af96ef90f5b61eb8eb0f7e77907cfd66613
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-05-18 17:42:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1076 ms, Memory: 99144 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static LinkedList<Integer>[] graph;
+	static int[] visited;
+	static int count;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		
+		graph = new LinkedList[N+1];
+		visited = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
+			graph[i] = new LinkedList<>();
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			graph[u].add(v);
+			graph[v].add(u);
+		}
+		
+		for(int i=1; i<=N; i++) {
+			Collections.sort(graph[i]);
+		}
+		
+		count = 1;
+		bfs(R);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=1; i<visited.length; i++) {
+			sb.append(visited[i]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	static void bfs(int v) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.offer(v);
+		visited[v] = count++;
+		
+		while(!queue.isEmpty()) {
+			int vertexItem = queue.poll();
+			for(int vertex : graph[vertexItem]) {
+				if(visited[vertex] == 0) {
+					queue.offer(vertex);
+					visited[vertex] = count++;
+				}
+			}
+		}
+	}
+}

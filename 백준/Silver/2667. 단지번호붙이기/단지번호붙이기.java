9c7c60ac98546cd940cc7cee20156a83e45846c9
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-05-26 01:07:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,17 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 	
 	static boolean[][] visited;
 	static int[][] adjArray;
-	static int[] dan_g;
 	static int[] dirX = {0, 0, 1, -1 };
 	static int[] dirY = {1, -1, 0, 0 };
 	static int N;
-	static int count;
+	static int danjiCount;
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,7 +19,6 @@ public class Main {
 		
 		visited = new boolean[N][N];
 		adjArray = new int[N][N];
-		dan_g = new int[25*25];
 		
 		for(int i=0; i<N; i++) {
 			String data = br.readLine();
@@ -28,26 +27,24 @@ public class Main {
 			}
 		}
 		
-		count = 0;
 		StringBuilder sb = new StringBuilder();
+		ArrayList<Integer> danji = new ArrayList<>();
 		
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
 				if(adjArray[i][j] == 1 && !visited[i][j]) {
-					count++;
+					danjiCount = 0;
 					dfs(i, j);
+					danji.add(danjiCount);
 				}
 			}
 		}
 		
-		sb.append(count).append("\n");
+		sb.append(danji.size()).append("\n");
+		Collections.sort(danji);
 		
-		Arrays.sort(dan_g);
-		
-		for(int i=0; i<dan_g.length; i++) {
-			if(dan_g[i] != 0) {
-				sb.append(dan_g[i]).append("\n");
-			}
+		for(int i=0; i<danji.size(); i++) {
+			sb.append(danji.get(i)).append("\n");
 		}
 		
 		System.out.println(sb.toString());
@@ -55,7 +52,7 @@ public class Main {
 	
 	static void dfs(int x, int y) {
 		visited[x][y] = true;
-		dan_g[count]++;
+		danjiCount++;
 		
 		for(int i=0; i<4; i++) {
 			int newX = x + dirX[i];

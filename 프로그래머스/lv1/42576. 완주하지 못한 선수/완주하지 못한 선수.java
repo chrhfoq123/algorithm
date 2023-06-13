33f529118431b5f442b7788e41e26d71e6a4cc05
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-06-14 01:16:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.55 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,18 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        HashMap<String, Integer> map = new HashMap<>();
         String answer = "";
         
-        for(int i=0; i<completion.length; i++){
-            if(!participant[i].equals(completion[i])){
-                answer += participant[i];
-                return answer;
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        
+        for(String player : completion){
+            map.put(player, map.get(player) - 1);
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                answer = key;
             }
         }
         
-        answer = participant[participant.length-1];
         return answer;
 	}
 }

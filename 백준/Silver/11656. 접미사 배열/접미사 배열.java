243c168e52c2af6db2ba53e977b1b4e8ccf25e5f
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-10-02 16:44:52 +0900

    [Silver IV] Title: 접미사 배열, Time: 300 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] arr = new String[s.length()];
+        
+        for(int i=0; i<s.length(); i++){
+            arr[i] = s.substring(i, s.length());
+        }
+        
+        Arrays.sort(arr);
+        
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}

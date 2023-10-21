b5873b975e0e10cad55d43bca00a25adec40aea6
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-10-21 21:55:57 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	String[] s = br.readLine().split("");
+    	Arrays.sort(s, Collections.reverseOrder());
+    	
+    	for(String n : s) {
+    		System.out.print(n);
+    	}
+    }
+}

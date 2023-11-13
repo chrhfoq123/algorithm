43560d60bd25b3f53f6bce44092eef018d646356
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-13 15:42:11 +0900

    Update 잃어버린 괄호.java

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,29 +1,29 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer sub = new StringTokenizer(br.readLine(), "-");
-        int sum = Integer.MAX_VALUE;
-        
-        while(sub.hasMoreTokens()){
-            int temp = 0;
-            StringTokenizer add = new StringTokenizer(sub.nextToken(), "+");
-            
-            while(add.hasMoreTokens()){
-                temp += Integer.parseInt(add.nextToken());
-            }
-            
-            if(sum == Integer.MAX_VALUE){
-                sum = temp;
-            }else{
-                sum -= temp;
-            }
-        }
-        
-        System.out.println(sum);
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer sub = new StringTokenizer(br.readLine(), "-");
+    	
+    	int answer = Integer.MAX_VALUE;
+    	while(sub.hasMoreTokens()) {
+    		int sum = 0;
+    		StringTokenizer add = new StringTokenizer(sub.nextToken(), "+");
+    		
+    		while(add.hasMoreTokens()) {
+    			sum += Integer.parseInt(add.nextToken());
+    		}
+    		
+    		if(answer == Integer.MAX_VALUE) {
+    			answer = sum;
+    		}else {
+    			answer -= sum;
+    		}
+    	}
+    	
+    	System.out.println(answer);
     }
 }

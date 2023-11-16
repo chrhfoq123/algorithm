2729237ae3a5b01d6f7e04b081d1a6fdef45ef0d
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-16 18:07:08 +0900

    [level 1] Title: 체육복, Time: 0.47 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,13 +2,12 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        
-        answer = n - lost.length;
+        int answer = n - lost.length;
         
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
+        //여분이 있었지만 도난당해서 못 빌려주는 경우
         for(int i=0; i<lost.length; i++){
             for(int j=0; j<reserve.length; j++){
                 if(lost[i] == reserve[j]){
@@ -20,6 +19,7 @@ class Solution {
             }
         }
         
+        //여분이 있어서 빌려줄 수 있는 경우
         for(int i=0; i<lost.length; i++){
             for(int j=0; j<reserve.length; j++){
                 if(lost[i]-1 == reserve[j] || lost[i]+1 == reserve[j]){
@@ -29,6 +29,7 @@ class Solution {
                 }
             }
         }
+        
         return answer;
     }
 }

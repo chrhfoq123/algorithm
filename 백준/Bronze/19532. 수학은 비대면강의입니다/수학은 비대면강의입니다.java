80f871a939cff927b5ef67795f96fbc2e137ae5f
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-16 17:38:06 +0900

    Update 수학은 비대면강의입니다.java

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -4,22 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int d = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		int f = Integer.parseInt(st.nextToken());
-	
-		for(int i=-999; i<1000; i++) {
-			for(int j=-999; j<1000; j++) {
-				if((a*i)+(b*j)==c && (d*i)+(e*j)==f) {
-					System.out.print(i + " " + j);
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	int a = Integer.parseInt(st.nextToken());
+    	int b = Integer.parseInt(st.nextToken());
+    	int c = Integer.parseInt(st.nextToken());
+    	int d = Integer.parseInt(st.nextToken());
+    	int e = Integer.parseInt(st.nextToken());
+    	int f = Integer.parseInt(st.nextToken());
+    	
+    	for(int i=-999; i<=999; i++) {
+    		for(int j=-999; j<=999; j++) {
+    			if((a*i + b*j == c) && (d*i + e*j == f)) {
+    				System.out.print(i + " " + j);
+    				return;
+    			}
+    		}
+    	}
+    }
 }

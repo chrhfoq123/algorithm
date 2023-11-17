71b6debb9d8a721cec2a75541004aa55947ae17c
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-17 17:32:02 +0900

    [level 2] Title: 구명보트, Time: 8.69 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,15 +3,16 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        
         Arrays.sort(people);
         
-        int minIdx = 0;
-        for(int i=people.length-1; i>=minIdx; i--){
-            if(people[i] + people[minIdx] <= limit){
-                minIdx++;
+        int index = 0;
+        for(int i=people.length-1; i>=index; i--){
+            if(people[i] + people[index] <= limit){
                 answer++;
+                index++;                
             }else{
-                answer++;
+                answer++;                
             }
         }
         

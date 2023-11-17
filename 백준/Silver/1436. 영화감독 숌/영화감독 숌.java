f22c0f1cf93c0f6a09488d2c2db3604e9bcb8b71
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-17 17:04:28 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86540 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,21 +1,21 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int ruin = 666;
-        int count = 1;
-        
-        while(count != N){
-            ruin++;
-            if(String.valueOf(ruin).contains("666")){
-                count++;
-            }
-        }
-        
-        System.out.println(ruin);
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	int n = Integer.parseInt(br.readLine());
+    	
+    	int num = 666;
+    	int cnt = 1;
+    	while(cnt != n) {
+    		num++;
+    		if(String.valueOf(num).contains("666")) {
+    			cnt++;
+    		}
+    	}
+    	
+    	System.out.println(num);
     }
 }

57b3a5a6041dc5c781bba9f95b43996c81441bc3
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-30 18:38:17 +0900

    Update 알고리즘 수업 － 깊이 우선 탐색 2.java

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -1,61 +1,58 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.LinkedList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[] visited;
-	static LinkedList<Integer>[] graph;
-	static int count;
-	
-	public static void main(String[] args) throws IOException{
+    
+    public static int[] visited;
+    public static LinkedList<Integer>[] graph;
+    public static int count;
+    
+    public static void dfs(int v){
+        visited[v] = count++;
+        for(int vertex : graph[v]){
+            if(visited[vertex] == 0){
+                dfs(vertex);
+            }
+        }
+    }
+    
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
-		
-		graph = new LinkedList[N+1];
-		visited = new int[N+1];
-		
-		for(int i=1; i<=N; i++) {
-			graph[i] = new LinkedList<>();
-		}
-		
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			
-			graph[u].add(v);
-			graph[v].add(u);
-		}
-		
-		for(int i=1; i<=N; i++) {
-			Collections.sort(graph[i], Comparator.reverseOrder());
-		}
-		
-		count = 1;
-		dfs(R);
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i=1; i<visited.length; i++) {
-			sb.append(visited[i]).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
-	
-	static void dfs(int v) {
-		visited[v] = count++;
-		for(int vertex : graph[v]) {
-			if(visited[vertex] == 0) {
-				dfs(vertex);
-			}
-		}
-	}
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        
+        graph = new LinkedList[n+1];
+        visited = new int[n+1];
+        
+        for(int i=1; i<=n; i++){
+            graph[i] = new LinkedList<>();
+        }
+        
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+        
+        for(int i=1; i<=n; i++){
+            Collections.sort(graph[i], Comparator.reverseOrder());
+        }
+        
+        count = 1;
+        dfs(r);
+        
+        for(int i=1; i<visited.length; i++){
+            System.out.println(visited[i]);
+        }
+    }
 }

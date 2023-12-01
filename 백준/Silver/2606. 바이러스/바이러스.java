bc843ff0ce5e46247b62bfc4fff96bf48e77dd0b
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-01 20:12:33 +0900

    Update 바이러스.java

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,58 +1,49 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static LinkedList<Integer>[] graph;
-	static boolean[] visited;
-	static int count;
+	public static int count;
+	public static boolean[] visited;
+	public static LinkedList<Integer>[] graph;
 	
-	public static void main(String[] args) throws IOException{
+	public static void dfs(int v) {
+		visited[v] = true;
+		count++;
+		for(int vertex : graph[v]) {
+			if(visited[vertex] == false) {
+				dfs(vertex);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int computer = Integer.parseInt(br.readLine());
-		int pair = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
 		
-		graph = new LinkedList[computer+1];
-		visited = new boolean[computer+1];
+		graph = new LinkedList[n+1];
+		visited = new boolean[n+1];
 		
-		for(int i=1; i<=computer; i++) {
+		for(int i=1; i<=n; i++) {
 			graph[i] = new LinkedList<>();
 		}
 		
-		for(int i=0; i<pair; i++) {
+		for(int i=0; i<m; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int num1 = Integer.parseInt(st.nextToken());
-			int num2 = Integer.parseInt(st.nextToken());
+			int v1 = Integer.parseInt(st.nextToken());
+			int v2 = Integer.parseInt(st.nextToken());
 			
-			graph[num1].add(num2);
-			graph[num2].add(num1);
+			graph[v1].add(v2);
+			graph[v2].add(v1);
 		}
 		
 		count = 0;
-		bfs(1);
+		dfs(1);
 		
-		System.out.println(count);
-	}
-	
-	static void bfs(int v) {
-		Queue<Integer> queue = new LinkedList<>();
-		
-		queue.offer(v);
-		visited[v] = true;
-		
-		while(!queue.isEmpty()) {
-			int vertexItem = queue.poll();
-			for(int vertex : graph[vertexItem]) {
-				if(!visited[vertex]) {
-					queue.offer(vertex);
-					visited[vertex] = true;
-					count++;
-				}
-			}
-		}
+		System.out.print(count-1);
 	}
 }

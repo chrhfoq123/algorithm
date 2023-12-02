a93eed94fe24c86338e354af28342c9ac50ec12b
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-02 20:39:04 +0900

    Update 단지번호붙이기.java

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,68 +1,65 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Main {
-	
-	static boolean[][] visited;
-	static int[][] adjArray;
-	static int[] dirX = {0, 0, 1, -1 };
-	static int[] dirY = {1, -1, 0, 0 };
-	static int N;
-	static int danjiCount;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		visited = new boolean[N][N];
-		adjArray = new int[N][N];
-		
-		for(int i=0; i<N; i++) {
-			String data = br.readLine();
-			for(int j=0; j<data.length(); j++) {
-				adjArray[i][j] = data.charAt(j)-'0';
-			}
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		ArrayList<Integer> danji = new ArrayList<>();
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(adjArray[i][j] == 1 && !visited[i][j]) {
-					danjiCount = 0;
-					dfs(i, j);
-					danji.add(danjiCount);
-				}
-			}
-		}
-		
-		sb.append(danji.size()).append("\n");
-		Collections.sort(danji);
-		
-		for(int i=0; i<danji.size(); i++) {
-			sb.append(danji.get(i)).append("\n");
-		}
-		
-		System.out.println(sb.toString());
-	}
-	
-	static void dfs(int x, int y) {
-		visited[x][y] = true;
-		danjiCount++;
-		
-		for(int i=0; i<4; i++) {
-			int newX = x + dirX[i];
-			int newY = y + dirY[i];
-			
-			if(newX>=0 && newY>=0 && newX<N && newY<N) {
-				if(adjArray[newX][newY] == 1 && !visited[newX][newY]) {
-					dfs(newX, newY);
-				}
-			}
-		}
-	}
+public class Main{
+    
+    public static int n;
+    public static int count;
+    public static int[][] map;
+    public static boolean[][] visited;
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+    
+    public static boolean inRange(int x, int y){
+        return (x >=0 && x < n && y >= 0 && y < n);
+    }
+    
+    public static void dfs(int x, int y){
+        visited[x][y] = true;
+        count++;
+        
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(inRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1){
+                dfs(nx, ny);
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        
+        map = new int[n][n];
+        visited = new boolean[n][n];
+        
+        for(int i=0; i<n; i++){
+            String s = br.readLine();
+            for(int j=0; j<n; j++){
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map[i][j] == 1 && !visited[i][j]){
+                    count = 0;
+                    dfs(i, j);
+                    list.add(count);
+                }
+            }
+        }
+        
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
 }

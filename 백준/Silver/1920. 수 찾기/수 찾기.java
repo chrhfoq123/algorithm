cd413c1a2191070f89243e45e7df585846b41a80
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-15 22:54:57 +0900

    [Silver IV] Title: 수 찾기, Time: 600 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,50 +1,49 @@
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	
+	public static boolean binarySearch(int[] arr, int target, int start, int end) {
+		if(start > end) {
+			return false;
+		}
+		int mid = (start + end) / 2;
+		if(arr[mid] == target) {
+			return true;
+		}else if(arr[mid] > target) {
+			return binarySearch(arr, target, start, mid-1);
+		}else {
+			return binarySearch(arr, target, mid+1, end);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] arrN = new int[N];
-		StringTokenizer st;
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arrN[i] = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
+		int[] arr1 = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr1[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Arrays.sort(arrN);
-		
-		int M = Integer.parseInt(br.readLine());
+		Arrays.sort(arr1);
+
+		int m = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
+		for(int i=0; i<m; i++) {
 			int target = Integer.parseInt(st.nextToken());
-			if(binary_search(arrN, target, 0, N-1)) {
+			if(binarySearch(arr1, target, 0, n-1)) {
 				sb.append(1).append("\n");
 			}else {
 				sb.append(0).append("\n");
 			}
 		}
 		
-		System.out.println(sb.toString());
-	}
-	
-	static boolean binary_search(int[] arr, int find, int start, int end) {
-		if(start > end) {
-			return false;
-		}
-		int mid = (start + end)/2;
-		
-		if(arr[mid] == find) {
-			return true;
-		}else if(arr[mid] > find) {
-			return binary_search(arr, find, start, mid-1);
-		}else {
-			return binary_search(arr, find, mid+1, end);
-		}
+		System.out.print(sb);
 	}
 }

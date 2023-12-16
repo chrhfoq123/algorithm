f9fc0078d2f91b381cf698b3c416d6ecf28e9691
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-16 22:54:38 +0900

    Update 숫자 카드 2.java

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,41 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		HashMap<Integer,Integer> hashMap = new HashMap<>();
-		StringTokenizer st;
+		int[] arr = new int[N];
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			
-			if(!hashMap.containsKey(num)) {
-				hashMap.put(num, 1);
-			}else {
-				hashMap.put(num, hashMap.get(num)+1);
-			}
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		Arrays.sort(arr);
 		
 		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		
+		st = new StringTokenizer(br.readLine()," ");
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
-			int target = Integer.parseInt(st.nextToken());
-			
-			if(hashMap.containsKey(target)) {
-				sb.append(hashMap.get(target) + " ");
-			}else {
-				sb.append(0 + " ");
-			}
+		for(int i = 0; i < M; i++) {
+			int key = Integer.parseInt(st.nextToken());
+
+			// upperBound와 lowerBound의 차이 값을 구한다.
+			sb.append(upperBound(arr, key) - lowerBound(arr, key)).append(' ');
 		}
 		
-		System.out.print(sb.toString());
+		System.out.println(sb);
+	}
+ 
+	private static int lowerBound(int[] arr, int key) {
+		int lo = 0; 
+		int hi = arr.length; 
+ 
+		// lo가 hi랑 같아질 때 까지 반복
+		while (lo < hi) {
+			int mid = (lo + hi) / 2; // 중간위치를 구한다.
+			/*
+			 *  key 값이 중간 위치의 값보다 작거나 같을 경우
+			 *  
+			 *  (중복 원소에 대해 왼쪽으로 탐색하도록 상계를 내린다.)
+			 */
+			if (key <= arr[mid]) {
+				hi = mid;
+			}else {
+				lo = mid + 1;
+			}
+		}
+ 
+		return lo;
+	}
+ 
+	private static int upperBound(int[] arr, int key) {
+		int lo = 0; 
+		int hi = arr.length; 
+ 
+		// lo가 hi랑 같아질 때 까지 반복
+		while (lo < hi) {
+			int mid = (lo + hi) / 2; // 중간위치를 구한다.
+			// key값이 중간 위치의 값보다 작을 경우
+			if (key < arr[mid]) {
+				hi = mid;
+			}else { // 중복원소의 경우 else에서 처리된다.
+				lo = mid + 1;
+			}
+		}
+ 
+		return lo;
 	}
 }

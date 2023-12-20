13cf5cd5b7eca0e350879cb613e78e02218a30fb
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-20 18:50:52 +0900

    [Gold I] Title: K번째 수, Time: 160 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int k = Integer.parseInt(br.readLine());
+		
+		long min = 1;
+		long max = k;
+		
+		while(min < max) {
+			long mid = (min + max) / 2;
+			long count = 0;
+			
+			for(int i=1; i<=n; i++) {
+				count += Math.min(mid/i, n);
+			}
+			
+			if(k <= count) {
+				max = mid;
+			}else {
+				min = mid + 1;
+			}
+		}
+		
+		System.out.println(min);
+	}
+}

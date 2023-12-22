f2c13014da2b56fa84456d0627ddc8e2a18bc160
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-22 21:10:16 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 504 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,42 +1,42 @@
-import java.util.Scanner;
-
-public class Main{
-    
-    public static int n;
-    public static int[] f;
-    public static int cnt = 0;
-    public static int dpCnt = 0;
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        f = new int[n+1];
-        
-        fib(n);
-        fibonacci(n);
-        
-        System.out.print(cnt + " ");
-        System.out.print(dpCnt);
-    }
-    
-    public static int fib(int n){
-        if(n == 1 || n == 2){
-            cnt++;
-            return 1;
-        }else{
-            return (fib(n - 1) + fib(n - 2));
-        }
-    }
-    
-    public static int fibonacci(int n){
-        f[1] = 1;
-        f[2] = 1;
-        
-        for(int i=3; i<=n; i++){
-            dpCnt++;
-            f[i] = f[i-1] + f[i-2];
-        }
-        
-        return f[n];
-    }
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	
+	public static int n;
+	public static int[] d;
+	public static int count = 0;
+	public static int dpCount = 0;
+	
+	public static int fib(int n) {
+		if(n == 1 || n == 2) {
+			count++;
+			return 1;
+		}else {
+			return fib(n-1) + fib(n-2);
+		}
+	}
+	
+	public static int fibonacci(int n) {
+		d[1] = 1;
+		d[2] = 1;
+		for(int i=3; i<=n; i++) {
+			dpCount++;
+			d[i] = d[i-1] + d[i-2];
+		}
+		
+		return d[n];
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		d = new int[n+1];
+		
+		fib(n);
+		fibonacci(n);
+		
+		System.out.println(count + " " + dpCount);
+	}
 }

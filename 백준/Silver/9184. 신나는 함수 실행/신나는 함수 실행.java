b799a8c367d2bf8d8311d029748869c461e4c30c
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-23 20:18:30 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 372 ms, Memory: 25200 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -1,42 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
-public class Main {
-	
-	public static int[][][] dp = new int[21][21][21];
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		while(true) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			
-			if(a == -1 && b == -1 && c == -1) {
-				break;
-			}
-			
-			System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
-		}
-	}
-	
-	public static int w(int a, int b, int c) {
-		if(a<=0 || b<=0 || c<=0) {
-			return 1;
-		}
-		
-		if(a>20 || b>20 || c>20) {
-			return w(20, 20, 20);
-		}
-		
-		if(dp[a][b][c] != 0) {
-			return dp[a][b][c];
-		}
-		
-		if(a<b && b<c) {
-			return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
-		}
-		
-		return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
-	}
+public class Main{
+    
+    public static int dp[][][] = new int[21][21][21];
+    
+    public static int w(int a, int b, int c){
+        if(a <= 0 || b <= 0 || c <= 0){
+            return 1;
+        }
+        
+        if(a > 20 || b > 20 || c > 20){
+            return w(20, 20, 20);
+        }
+        
+        if(dp[a][b][c] != 0){
+            return dp[a][b][c];
+        }
+        
+        if(a < b && b < c){
+            return dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+        }
+        
+        return dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            if(a == -1 && b == -1 && c == -1){
+                break;
+            }
+            
+            int result = w(a, b, c);
+            sb.append("w(" + a + ", " + b + ", " + c + ") = " + result).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
 }

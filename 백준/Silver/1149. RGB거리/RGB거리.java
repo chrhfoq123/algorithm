7a6eecb8140e3de054113a14e72782aba86f68a1
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-12-25 17:18:17 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args){
-    	Scanner sc = new Scanner(System.in);
-    	int n = sc.nextInt();
-    	int[][] dp = new int[n+1][3];
-
-    	for(int i=1; i<=n; i++) {
-    		int r = sc.nextInt();
-    		int g = sc.nextInt();
-    		int b = sc.nextInt();
-    		
-    		dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + r;
-    		dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + g;
-    		dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + b;
-    	}
-    	
-    	System.out.print(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])));
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n][3];
+        
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            dp[i][0] = Integer.parseInt(st.nextToken());
+            dp[i][1] = Integer.parseInt(st.nextToken());
+            dp[i][2] = Integer.parseInt(st.nextToken());
+        }
+        
+        //0번 집의 비용은 정해져 있기 때문에 다음인 1부터
+        for(int i=1; i<n; i++){
+            dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
+            dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
+            dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
+        }
+        
+        System.out.print(Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]));
     }
 }
